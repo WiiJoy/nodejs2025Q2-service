80d733c3f08e3f42b6cda7d28ff84335f877d680
Vitalii Rogozin <wiification@gmail.com>
2025-05-30 15:34:56 +0500

    feat: add other routes by nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module'
 import { UserModule } from './user/user.module'
 import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, ArtistModule],
+  imports: [DatabaseModule, UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
   controllers: [AppController],
   providers: [AppService],
 })
