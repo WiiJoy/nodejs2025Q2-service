b72cf823d2aa12dc7ca78eec0cefeb37701801bd
Vitalii Rogozin <wiification@gmail.com>
2025-06-02 00:12:14 +0500

    refactor: remove commented code

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -13,8 +13,6 @@ import {
   HttpCode,
 } from '@nestjs/common';
 import { ArtistService } from './artist.service';
-// import { CreateArtistDto } from './dto/create-artist.dto';
-// import { UpdateArtistDto } from './dto/update-artist.dto';
 import { errors, ArtistCreate } from 'src/types';
 import { validate } from 'uuid';
 
