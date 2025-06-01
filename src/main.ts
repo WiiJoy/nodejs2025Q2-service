81bc727b1753f4886807bdcae7b8eadd923fbff2
Vitalii Rogozin <wiification@gmail.com>
2025-06-01 10:17:59 +0500

    feat: add swagger for api doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { readFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { config } from 'dotenv';
+import { load } from 'js-yaml';
+
+config();
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const apiLink = resolve(__dirname, '..', 'doc', 'api.yaml');
+  const doc = await readFile(apiLink, 'utf8');
+  SwaggerModule.setup('doc', app, load(doc) as OpenAPIObject);
+  await app.listen(PORT);
 }
 bootstrap();
